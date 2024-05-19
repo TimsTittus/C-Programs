1db782832c4d7a8d43423bb3e363c21f58a35f74
Tims Tittus <151863700+TimsTittus@users.noreply.github.com>
2024-05-19 09:16:05 +0530

    Add files via upload

---
+++ convert metre to centimetre.c
@@ -0,0 +1,12 @@
+//to convert metre to centimetre
+#include<stdio.h>
+void main()
+{
+    float m,c;
+    printf("Enter the metre to be converted : ");
+    scanf("%f",&m);
+    c=m*100;
+    printf("The output  value is %f cm",c);
+    
+    return 0;
+}
