64f9d0b0dbd8ac1ff7ead5e519191943a9cc7d68
Tims Tittus <151863700+TimsTittus@users.noreply.github.com>
2024-05-29 21:02:33 +0530

    Add files via upload

---
+++ multiply two numbers.c
@@ -0,0 +1,12 @@
+//To "Multiply" two numbers
+#include<stdio.h>
+void main()
+{
+    float a,b,m=0;
+    printf("Enter the first number : ");
+    scanf("%f",&a);
+    printf("Enter the second number : ");
+    scanf("%f",&b);
+    m=a*b;
+    printf("The product of two numbers is :%f",m);
+}
