4394cbe4fe75903580efa9b73c175071191d6bf6
Tims Tittus <151863700+TimsTittus@users.noreply.github.com>
2024-06-03 20:41:10 +0530

    Add files via upload

---
+++ sum of two numbers.c
@@ -0,0 +1,12 @@
+//To find the sum of two numbers in C program
+#include<stdio.h>
+void main()
+{
+    float a,b,sum=0;
+	printf("Enter the first number : ");
+	scanf("%f",&a);
+	printf("Enter the second number : ");
+	scanf("%f",&b);
+	sum=a+b;
+	printf("Sum = %f",sum);
+}
