1a60af4463a59ef2eeaa64c5ed435605f7f15112
Tims Tittus <151863700+TimsTittus@users.noreply.github.com>
2024-06-04 20:39:28 +0530

    Add files via upload

---
+++ substract two numbers.c
@@ -0,0 +1,12 @@
+//C program to "substract two numbers"
+#include<stdio.h>
+void main()
+{
+    float a,b,subs=0;
+    printf("Enter the first number : ");
+    scanf("%f",&a);
+    printf("Enter the second number ; ");
+    scanf("%f",&b);
+    subs=a-b;
+    printf("Output of substraction = %f",subs);
+}
